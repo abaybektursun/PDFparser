8a4562b1e2dc71ff2cb2c14b6fa98993e4145ef9
unknown <ABEKTURSUN@STLVDBUS013.CENTENE.com>
2015-12-28 11:28:19 -0600

    Fully working shifter and clean layout writer

--- tide.py
+++ tide.py
@@ -2,9 +2,11 @@
 import re
 #/LOElement(.+?)/g
 
+import os
 from os      import listdir
 from os.path import isfile, join,exists
 from os      import makedirs
+from os      import walk
 
 #debug###############
 recursionStack = 0
@@ -47,10 +49,10 @@ def shifter(line,layoutElements, recursionStack):
     if len(layoutElements) > 0:
         replaceLine = shifter(replaceLine,layoutElements, recursionStack)
         
-    #debug#################DELETE
+    #debug##################################################
     #print('Stack #' + str(recursionStack) + ' Returned!\n')
     #print('Results: ' + '\n\n' + replaceLine)
-    #debug#################DELETE
+    #debug##################################################
     
     return replaceLine   
 
@@ -62,17 +64,39 @@ for element in tempLayoutElements:
     layoutElements.append(element + '(')
 
 # Clean All the Pages
-outFileList     = []
-layoutPath      = r'C:\Projects\PDFparser\pageLayout\05_28_2015_Blankenship_MD_Charles'
-layoutFileNames = [f for f in listdir(layoutPath) if isfile(join(layoutPath, f))]
-layoutFile      = open(r'C:\Projects\PDFparser\pageLayout\05_28_2015_Blankenship_MD_Charles\1.pmlo')
+outPageList = []
+layoutPath  = r'C:\Projects\PDFparser\pageLayout'
+allLayouts  = [x[0] for x in os.walk(layoutPath)]
+allLayouts.pop(0)
 
-for index, line in enumerate(layoutFile):
-    layoutElementsSwapper = list(layoutElements)
-    outFileList.append(shifter(line,layoutElements, recursionStack))
-    layoutElements = list(layoutElementsSwapper)
+for a_layoutFolderPath in allLayouts:
 
-   
-for line in outFileList:
-    #outlayoutFiles.write(line)
-    print(line)
+    #debug###########################
+    #print('\n' + a_layoutFolderPath)
+    #debug###########################
+
+    cleanPageLayoutPath = r'C:\Projects\PDFparser\cleanPageLayout'
+    cleanLayoutName     = a_layoutFolderPath.split('\\')[-1]
+    layoutPages         = [f for f in listdir(a_layoutFolderPath) if isfile(join(a_layoutFolderPath, f))]
+    
+    if not os.path.exists(cleanPageLayoutPath + '\\' + cleanLayoutName):
+        os.makedirs(cleanPageLayoutPath + '\\' + cleanLayoutName)
+    
+    for a_page in layoutPages:
+    
+        #debug###############
+        #print('\t' + a_page)
+        #debug###############
+        
+        layoutFile = open(a_layoutFolderPath + '\\' + a_page)
+        for index, line in enumerate(layoutFile):
+            layoutElementsSwapper = list(layoutElements)
+            outPageList.append(shifter(line,layoutElements, recursionStack))
+            layoutElements = list(layoutElementsSwapper)
+        
+        outPageFile = open(cleanPageLayoutPath + '\\' + cleanLayoutName + '\\' + a_page, 'w')
+        
+        for line in outPageList:
+            outPageFile.write(str(line))
+            
+        outPageList = []
