cbf56f22aea8d6889d06f0ded0e26fe49647c3c6
unknown <ABEKTURSUN@STLVDBUS013.CENTENE.com>
2015-12-30 17:24:26 -0600

    Working GUI debug, and working cleaner

--- tide.py
+++ tide.py
@@ -2,7 +2,7 @@
 import os
 import re
 import cPickle as pickle
-
+import tkFont
 
 #Temp
 from Tkinter import *
@@ -18,19 +18,13 @@ from time    import sleep
 # PDF Miner Layout Object 
 class PMLO:
     def __init__(self, rawData):
-        self.y       = 0.0
-        self.x       = 0.0
-        self.width   = 0.0
-        self.height  = 0.0
-        self.content = ''
-        self.type    = ''
-        
+        self.content = ''        
         self.type    = rawData.split('(')[0]
         coords       = rawData.split('(')[1].replace(')','').split(',')
-        self.x       = float(coords[0])
-        self.y       = float(coords[1])
-        self.width   = float(coords[2])
-        self.height  = float(coords[3])
+        self.width   = float(coords[0])
+        self.height  = 793.6801 - float(coords[1])
+        self.x       = float(coords[2])
+        self.y       = 793.6801 - float(coords[3])
                      
 #debug##self.height#############
 recursionStack = 0
@@ -92,6 +86,13 @@ layoutPath  = r'C:\Projects\PDFparser\pageLayout'
 allLayouts  = [x[0] for x in os.walk(layoutPath)]
 allLayouts.pop(0)
 
+# GUI DEBUG
+root = Tk()
+y_dimension = 793.6801
+frame = Canvas(root, width=617.7601, height=y_dimension)
+frame.pack()
+root.update_idletasks()
+#
 for a_layoutFolderPath in allLayouts:
 
     #debug###########################
@@ -151,39 +152,54 @@ for a_layoutFolderPath in allLayouts:
                     #debug
                     #print(found.group())
                     #debug
-                    
+
                     break
-                
+
                 if counter + 1 == len(layoutElements):
                     if len(listPMLO) != 0: 
                         listPMLO[current_obj].content += line
 
-        # Write the results to a file
+        # Write the results to a file (useless)
         for line in outPageListLines:
             outPageFile.write(str(line))
-        
+
         # Debug ##################
-        root = Tk()
-        y_dimension = 793.6801
-        frame = Canvas(root, width=617.7601, height=y_dimension)
-        frame.pack()
-        root.update_idletasks()
-        
+
+
+        for index, an_obj in enumerate(listPMLO[:]):
+            if an_obj.x > 600 or an_obj.x < 20:
+                listPMLO.remove(an_obj)
+            elif an_obj.width > 600 or an_obj.width < 20:
+                listPMLO.remove(an_obj)
+            elif an_obj.y < 17 or an_obj.y > 780:
+                listPMLO.remove(an_obj) 
+            elif an_obj.height < 17 or an_obj.height > 780:
+                listPMLO.remove(an_obj)
+                
         for index, an_obj in enumerate(listPMLO):
-            sleep(0.1)
-            the_color = '#' + str( (306000 + index * 3) )
+            #sleep(0.01)
+            the_color = '#' + str( (906000 + index * 3) )
             #frame.delete("all")
-            frame.create_rectangle(an_obj.width, an_obj.height, an_obj.x, an_obj.y, fill=the_color)
+            # clear the big objects|clear the right side|clear 20px from bottom to top            
+
+            #frame.create_rectangle(an_obj.width, an_obj.height, an_obj.x, an_obj.y, fill=the_color)
+            #if an_obj.content == '' or an_obj.content == '\n':
+            frame.create_rectangle(an_obj.x, an_obj.y, an_obj.width, an_obj.height, fill=the_color)
+            frame.create_text(an_obj.x, an_obj.y, text=an_obj.content, anchor=NE, font=tkFont.Font(family='Helvetica', size=7, weight='bold') )
+            
+
             # force widget to display
             frame.pack()
             # force refresh of the widget to be sure that thing are displayed
             frame.update_idletasks()
             #print(an_obj.content)
-
+        eshek = 'cd' + raw_input('Ne nah?: ')
         #root.quit()
-        root.destroy()
+        frame.delete("all")
+
+        #root.destroy()
         # Debug ##################
-        
+
         del outPageList[:]
         del outPageListLines[:]
 
